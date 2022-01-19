965410ea6aa93848729811da4b9ec7dc7726a88c
MoElazazy07 <m.mamdouh@softexsw.com>
2022-01-20 01:46:15 +0200

    Add project files.

---
+++ Customer/Controllers/CustomerController.cs
@@ -0,0 +1,62 @@
+﻿using Customer.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Customer.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult Get()
+        {
+
+            using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["CustomerConnection"].ConnectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(@"SELECT  [StockId],[StockCode],[LastPrice],[Name]FROM [Customer].[dbo].[TblStock]", connection))
+                {
+                    // Make sure the command object does not already have
+                    // a notification object associated with it.
+                    command.Notification = null;
+
+                    SqlDependency dependency = new SqlDependency(command);
+                    dependency.OnChange += new OnChangeEventHandler(dependency_OnChange);
+
+                    if (connection.State == ConnectionState.Closed)
+                        connection.Open();
+
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    var listCus = reader.Cast<IDataRecord>()
+                            .Select(x => new
+                            {
+                                StockId = (int)x["StockId"],
+                                StockCode = (string)x["StockCode"],
+                                LastPrice = (int)x["LastPrice"],
+                                Name = (string)x["Name"],
+
+                            }).ToList();
+
+                    return Json(new { listCus = listCus }, JsonRequestBehavior.AllowGet);
+
+                }
+            }
+        }
+
+        private void dependency_OnChange(object sender, SqlNotificationEventArgs e)
+        {
+            CusHub.Show();
+        }
+    }
+}
